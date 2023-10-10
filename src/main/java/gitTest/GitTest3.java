c361d2765b6e903bde85e169e9483d12086c48ba
lilprincess19 <143635095+lilprincess19@users.noreply.github.com>
2023-10-10 17:08:53 -0500

    Committed 3rd

---
+++ src/main/java/gitTest/GitTest3.java
@@ -0,0 +1,31 @@
+package gitTest;
+
+public class GitTest3 {
+	public static void main (String[]args) {
+		printMe();
+		printMe1();
+		printMe2();
+		
+		
+
+}
+	public static void printMe() {
+		System.out.println("gitTest");
+		
+		
+	}
+	public static void printMe1() {
+		System.out.println("git Test1 ");
+		
+
+}
+	public static void printMe2() {
+		System.out.println("git Test2");
+		
+}
+	public static void printMe3() {
+		
+	}
+	public static void printMe4() {
+}
+}
