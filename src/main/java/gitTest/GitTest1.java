96b84e53b723cb7d6b1a4c96656d28097dc04c9c
lilprincess19 <143635095+lilprincess19@users.noreply.github.com>
2023-10-09 22:31:39 -0500

    testclass

---
+++ src/main/java/gitTest/GitTest1.java
@@ -0,0 +1,27 @@
+package gitTest;
+
+public class GitTest1 {
+	public static void main (String[]args) {
+		printMe();
+		printMe1();
+		printMe2();
+		
+		
+
+}
+	public static void printMe() {
+		System.out.println("gitTest");
+		
+		
+	}
+	public static void printMe1() {
+		System.out.println("git Test1 ");
+		
+
+}
+	public static void printMe2() {
+		System.out.println("git Test2");
+		
+		
+	}
+}
